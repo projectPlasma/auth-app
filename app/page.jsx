03126bd539ed0b4dcde6263fb38fe4e2be4aa65e
Kwadwo Asare <worldkwadwo@yahoo.com>
2023-11-20 18:57:57 -0800

    second commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div>Hello Auth world!</div>
+    </main>
+  );
+};
+
+export default Home;
+
+// GTCoding => 5:44 => "Designing UI Using Tailwind CSS"
